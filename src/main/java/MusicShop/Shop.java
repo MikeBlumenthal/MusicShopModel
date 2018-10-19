49fde036a8436601edef841fc3b7aceb8843597b
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-19 16:30:55 +0100

    sets up shop class

--- src/main/java/MusicShop/Shop.java
+++ src/main/java/MusicShop/Shop.java
@@ -1,4 +1,48 @@
 package MusicShop;
 
+import MusicShop.Interfaces.Sellable;
+
+import java.util.ArrayList;
+
 public class Shop {
+    String name;
+    double till;
+    ArrayList<Sellable> stock;
+
+    public Shop(String name, double till){
+        this.name = name;
+        this.till = till;
+        this.stock = new ArrayList<>();
+    }
+
+    public ArrayList<Sellable> getStock() {
+        return stock;
+    }
+
+    public void addToStock(Sellable item){
+        stock.add(item);
+    }
+
+    public void removeFromStock(Sellable item){
+        stock.remove(item);
+    }
+
+    public void sellItem(Sellable item){
+        removeFromStock(item);
+        till += item.getSellingPrice();
+    }
+
+    public double getTotalProfit(){
+        //iterate over the stock arraylist,
+        //calling calculateMarkup on each element and
+        // adding to a total profit variable which will be returned
+        double totalProfit = 0;
+        for (Sellable item:stock
+             ) {
+            totalProfit += item.calculateMarkUp();
+        }
+        return totalProfit;
+    }
+
+
 }
